2c20608b2de65a0bd9d6b17858db066f65fae260
manohar chippada <manoharchippada@MacBookPro.lan>
2025-08-20 23:08:27 -0400

    added all changes

--- email_form.py
+++ email_form.py
@@ -41,7 +41,11 @@ if total_calories > target:
 
     # Create MIME message
     msg = MIMEMultipart("alternative")
+<<<<<<< HEAD
     msg["Subject"] = f"🔥 Calorie Alert! You’ve hit {{total_calories}} kcal today"
+=======
+    msg["Subject"] = f"🔥 Calorie Alert! You’ve hit {total_calories} kcal today"
+>>>>>>> de9cb12 (added all changes)
     msg["From"] = sender
     msg["To"] = recipient
     msg.attach(MIMEText(html_content, "html"))
